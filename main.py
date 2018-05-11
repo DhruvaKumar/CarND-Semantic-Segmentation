0297715a3efad8c9adb4860a199c602392028e7b
DhruvaKumar <dhruva.kumar08@gmail.com>
2018-05-11 01:51:42 -0400

    testing flow with 1 epoch

--- main.py
+++ main.py
@@ -169,8 +169,8 @@ def run():
     correct_label = tf.placeholder(tf.float32, shape=[None, *image_shape, num_classes])
     learning_rate = tf.placeholder(tf.float32, shape=[])
 
-    epochs = 10
-    batch_size = 5
+    epochs = 1
+    batch_size = 289
 
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
@@ -194,12 +194,12 @@ def run():
         _, train_op, cross_entropy_loss = optimize(output, correct_label,
          learning_rate, num_classes)
         
-        # TODO: Train NN using the train_nn function
+        #  Train NN using the train_nn function
         train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss,
          input_image, correct_label, keep_prob, learning_rate)
 
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        # Save inference data using helper.save_inference_samples
+         helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, output, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
